c4b012065e2a26bfe5fc89b471b69183295e1195
Tirth Chaudhary <tirth@Tirths-MacBook-Air.local>
2025-05-30 19:23:22 +0530

    added functionality to forward request from lb to server1

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	lb := setupLoadBalancer()
+	server1 := setupServer1()
+	go lb.Run(":8080")
+	go server1.Run(":8081")
+
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+	<-sigChan
+	log.Println("Shutting down gracefully...")
+
 }
